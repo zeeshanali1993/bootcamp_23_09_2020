c5ffe662d63e4e5593594561366a11c83d6502b4
Zeeshan Ali <zeeshanali93@outlook.com>
2020-09-23 18:20:56 +0500

    Update childObject.js

--- src/childObject.js
+++ src/childObject.js
@@ -5,6 +5,7 @@ function ReceiveValue(props)
     return (
         <div>
             <h1>Today we have {props.dishName1} and {props.dishName2}</h1>
+            <h1>Published through actions</h1>
         </div>
     );
 }
